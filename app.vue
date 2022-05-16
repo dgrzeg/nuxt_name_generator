9b2dabb0fca9c91e236c1bad44a13a515b94cb3c
dgrzeg <dgr.wroc@gmail.com>
2022-05-16 17:09:00 +0200

    names component, emit handling

--- app.vue
+++ app.vue
@@ -12,10 +12,13 @@
       <button class="primary" @click="computeSelectedNames">Find Names</button>
     </div>
     <div class="cards-container">
-      <div v-for="name in selectedNames" :key="name" class="card">
-        <h4>{{ name }}</h4>
-        <p>x</p>
-      </div>
+      <CardName
+        v-for="(name, index) in selectedNames"
+        :key="name"
+        :name="name"
+        @remove="removeName"
+        :index="index"
+      />
     </div>
   </div>
 </template>
@@ -42,6 +45,13 @@ const computeSelectedNames = () => {
 }
 
 const selectedNames = ref<string[]>([])
+
+const removeName = (index: number) => {
+  //selectedNames.value.splice(index, 1)
+  const tempNames = [...selectedNames.value]
+  tempNames.splice(index, 1)
+  selectedNames.value = tempNames
+}
 
 const optionsArray = [
   {
@@ -97,22 +107,5 @@ h1 {
   display: flex;
   margin-top: 3rem;
   flex-wrap: wrap;
-}
-.card {
-  background-color: rgb(27, 60, 138);
-  width: 28%;
-  color: white;
-  border-radius: 1rem;
-  padding: 0.1rem;
-  margin-right: 0.5rem;
-  margin-bottom: 1rem;
-  position: relative;
-}
-.card p {
-  position: absolute;
-  top: -29%;
-  left: 92.5%;
-  cursor: pointer;
-  color: rgba(255, 255, 255, 0.178);
 }
 </style>
