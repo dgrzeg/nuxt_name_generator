91a19692c9f1b449f1ad158e507f02307258fd5d
dgrzeg <dgr.wroc@gmail.com>
2022-05-16 14:18:40 +0200

    html&styles

--- app.vue
+++ app.vue
@@ -1,3 +1,125 @@
 <template>
-  <div></div>
+  <div class="container">
+    <h1>Baby Name Generator</h1>
+    <p>Choose your options and click the "Find Names" buttom below</p>
+    <div class="options-container">
+      <div class="option-container">
+        <h4>1) Choose a gender</h4>
+        <div class="option-buttons">
+          <button
+            class="option"
+            :class="options.gender === 'Boy' && 'option-active'"
+          >
+            Boy
+          </button>
+          <button
+            class="option"
+            :class="options.gender === 'Unisex' && 'option-active'"
+          >
+            Unisex
+          </button>
+          <button
+            class="option"
+            :class="options.gender === 'Girl' && 'option-active'"
+          >
+            Girl
+          </button>
+        </div>
+      </div>
+      <div class="option-container">
+        <h4>2) Choose the name's popularity</h4>
+        <div class="option-buttons">
+          <button
+            class="option"
+            :class="options.popularity === 'Trendy' && 'option-active'"
+          >
+            Trendy
+          </button>
+          <button
+            class="option"
+            :class="options.popularity === 'Unique' && 'option-active'"
+          >
+            Unique
+          </button>
+        </div>
+      </div>
+      <div class="option-container">
+        <h4>2) Choose name's length</h4>
+        <div class="option-buttons">
+          <button
+            class="option"
+            :class="options.length === 'Long' && 'option-active'"
+          >
+            Long
+          </button>
+          <button
+            class="option"
+            :class="options.length === 'All' && 'option-active'"
+          >
+            All
+          </button>
+          <button
+            class="option"
+            :class="options.length === 'Short' && 'option-active'"
+          >
+            Short
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script setup lang="ts">
+const options = reactive({
+  gender: 'Girl',
+  popularity: 'Trendy',
+  length: 'All',
+})
+</script>
+
+<style scoped>
+.container {
+  font-family: Arial, Helvetica, sans-serif;
+  color: rgb(27, 60, 138);
+  max-width: 50rem;
+  margin: 0 auto;
+  text-align: center;
+}
+h1 {
+  font-size: 3rem;
+}
+.options-container {
+  background-color: rgb(255, 238, 236);
+  border-radius: 2rem;
+  padding: 1rem;
+  width: 95%;
+  margin: 0 auto;
+  margin-top: 4rem;
+  position: relative;
+}
+.option-container {
+  margin-bottom: 2rem;
+}
+.option {
+  background: white;
+  outline: 0.15rem solid rgb(249, 87, 89);
+  border: none;
+  padding: 0.75rem;
+  width: 12rem;
+  font-size: 1rem;
+  color: rgb(27, 60, 138);
+  cursor: pointer;
+  font-weight: 200;
+}
+.option:first-of-type {
+  border-radius: 1rem 0 0 1rem;
+}
+.option:last-of-type {
+  border-radius: 0 1rem 1rem 0;
+}
+.option-active {
+  background-color: rgb(249, 87, 89);
+  color: white;
+}
+</style>
